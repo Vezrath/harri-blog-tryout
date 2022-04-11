3c6c372e78ff20a5f815a6047630a2288004c838
Vezrath <harri.moisala@gmail.com>
2022-04-11 08:12:12 +0300

    Changing from SQLite to Postgres

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy import Column
 from sqlalchemy.orm import relationship
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
@@ -28,7 +29,7 @@ gravatar = Gravatar(app,
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
